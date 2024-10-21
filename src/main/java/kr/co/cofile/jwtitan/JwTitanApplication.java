41be120ecf009bf58fcfd3d873f12b5047101573
shinis(home) <actionshin@gmail.com>
2024-10-21 21:11:26 +0900

    의존성 패키지 설치

---
+++ src/main/java/kr/co/cofile/jwtitan/JwTitanApplication.java
@@ -0,0 +1,13 @@
+package kr.co.cofile.jwtitan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwTitanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwTitanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/cofile/jwtitan/JwTitanApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.cofile.jwtitan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwTitanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
